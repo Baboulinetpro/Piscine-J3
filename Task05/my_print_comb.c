d3d27af731a30c8463dd196afa651cf707ce8ebc
Antoine Nattier <antoinenattier@macbook-pro-de-antoine.home>
2021-02-05 15:18:34 +0100

    Task05

---
+++ Task05/my_print_comb.c
@@ -0,0 +1,23 @@
+int my_print_comb(void)
+{
+  int a = 48, b = 48, c= 48;
+    while (a <= 57)
+    {
+      b = a +1;
+      while (b <= 57)
+	{
+	c = b +1;
+	while (c <= 57)
+	  {
+	   my_putchar(a);
+           my_putchar(b);
+           my_putchar(c);
+           my_putchar('\n');
+	      c++;
+	  }
+	b++;
+	}
+      a++;
+    }
+    return(0);
+}
